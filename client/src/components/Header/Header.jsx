db29ecec7d7b386738156f27341f6f6cf2d713ae
Smoothie <lavaboom@yahoo.com>
2022-01-20 23:22:06 -0500

    added header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,13 +1,34 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import './Header.scss'
+import iconCoach from '../../assets/Icons/coach.svg'
+import iconMenu from '../../assets/Icons/menu.svg'
+import iconChevronDown from '../../assets/Icons/chevron-down.svg'
 
 export default class Header extends Component {
 
     render() {
         return (
-            <header>
-                <p>This is Header</p>
+            <header className='header'>
+                <div className='header-buttons'>
+                    <div className='header-buttons__icon'>
+                        <img src={ iconCoach } alt='left icon' />
+                    </div>
+                    <div className='header-buttons__dates'>
+                        <div>TODAY <img className='header-buttons__icon' src={ iconChevronDown } alt='down' /></div>
+                    </div>
+                    <div className='header-buttons__icon'>
+                    <img src={ iconMenu } alt='right icon' />
+                    </div>
+                </div>
+                <div className='header-nav'>
+                    <ul className='header-nav__menu'>
+                        <li className='header-nav__item'>OVERVIEW</li>
+                        <li className='header-nav__item header-nav__item--selected'>STRAIN</li>
+                        <li className='header-nav__item'>RECOVERY</li>
+                        <li className='header-nav__item'>SLEEP</li>
+                    </ul>
+                </div>
             </header>
         )
     }
