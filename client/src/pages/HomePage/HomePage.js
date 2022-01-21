1d86fbbf7698b67f0c1f36ee4b04fae69523c91a
Smoothie <lavaboom@yahoo.com>
2022-01-21 12:03:08 -0500

    added dynamic selected state for metrics

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -7,12 +7,14 @@ import logo from '../../assets/Icons/logo.svg';
 
 // other sub components
 import Card from '../../components/Card/Card'
+import MetricsPanel from '../../components/MetricsPanel/MetricsPanel'
 
 export default class HomePage extends Component {
     render() {
         return (
             <div className='home-page'>
                 <Charts metric="7.1" title="DAY STRAIN" btnText="No Activities" chartColor="#0072BC" />
+                <MetricsPanel/>
                 <Card icon={logo}
                     header='LIGHT EXERTION'
                     content='Strain between 6 and 9.9 is considered light. Your accumulated cardiovascular load is low.'
