c9878818b7197bd12f91b005e253f6b95d04dc15
Louis Flores <louisdefleurs@gmail.com>
2022-01-21 07:55:37 -0500

    getting started

--- client/src/components/Charts/Charts.js
+++ client/src/components/Charts/Charts.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Doughnut } from 'react-chartjs-2';
 import "./Charts.scss";
-import {Chart, ArcElement } from 'chart.js'
+import { Chart, ArcElement } from 'chart.js'
 // import ChartDataLabels from 'chartjs-plugin-datalabels';
 Chart.register(ArcElement);
 // Chart.register(ChartDataLabels);
@@ -9,29 +9,34 @@ Chart.register(ArcElement);
 
 export default function Charts() {
     const plugins = [{
-        beforeDraw: function(chart) {
+        beforeDraw: function (chart) {
             let width = chart.width,
-             height = chart.height,
-             ctx = chart.ctx;
-             ctx.restore();
-             let fontSize = (height / 160).toFixed(2);
-             ctx.font = fontSize + "em sans-serif";
-             ctx.textBaseline = "top";
-             let text = "Recovery",
-             textX = Math.round((width - ctx.measureText(text).width) / 2),
-             textY = height / 2;
-             ctx.fillText(text, textX, textY);
-             ctx.save();
-        } 
-      }]
+                height = chart.height,
+                ctx = chart.ctx;
+            ctx.restore();
+            let fontSize = (height / 160).toFixed(2);
+            ctx.font = fontSize + "em sans-serif";
+            ctx.textBaseline = "top";
+            let text = "Recovery",
+                textX = Math.round((width - ctx.measureText(text).width) / 2),
+                textY = height / 2;
+
+            // let text2 = "75%",
+            //     textX2 = Math.round((width - ctx.measureText(text).width) / 2),
+            //     textY2 = height / 2;
+            ctx.fillText(text, textX, textY);
+            // ctx.fillText(text2, textX2, textY2);
+            ctx.save();
+        }
+    }]
     const data = {
         datasets: [
             {
-                data: [75,25],
+                data: [75, 25],
                 borderColor: ['rgba(255,206,86,0.2)'],
-                backgroundColor: ['rgba(82,157,242,1)','rgba(75, 78, 83,1)'],
+                backgroundColor: ['rgba(82,157,242,1)', 'rgba(75, 78, 83,1)'],
             }
-            ],
+        ],
         text: 'day strain',
     };
     const options = {
@@ -41,10 +46,10 @@ export default function Charts() {
                 text: 'Custom Chart Title'
             }
         },
-        cutout:'80%',
-    } 
-    
-  return <div className='doughnut'>
-      <Doughnut data={data}  options={options} plugins={plugins}/>
-  </div>;
+        cutout: '80%',
+    }
+
+    return <div className='doughnut'>
+        <Doughnut data={data} options={options} plugins={plugins} />
+    </div>;
 }
