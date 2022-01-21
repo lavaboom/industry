66e8888ac2692b66802f1f16664900af65e05fb3
Louis Flores <louisdefleurs@gmail.com>
2022-01-21 06:45:54 -0500

    button reuseable

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,36 +1,48 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Button from "../Button/Button";
+
 // app styles & assets
-import './Header.scss'
-import iconCoach from '../../assets/Icons/coach.svg'
-import iconMenu from '../../assets/Icons/menu.svg'
-import iconChevronDown from '../../assets/Icons/chevron-down.svg'
+import "./Header.scss";
+import iconCoach from "../../assets/Icons/coach.svg";
+import iconMenu from "../../assets/Icons/menu.svg";
+import iconChevronDown from "../../assets/Icons/chevron-down.svg";
 
 export default class Header extends Component {
+	onBtnClick() {
+		console.warn("Whoop there it is");
+	}
 
-    render() {
-        return (
-            <header className='header'>
-                <div className='header-buttons'>
-                    <div className='header-buttons__icon'>
-                        <img src={ iconCoach } alt='left icon' />
-                    </div>
-                    <div className='header-buttons__dates'>
-                        <div>TODAY <img className='header-buttons__icon' src={ iconChevronDown } alt='down' /></div>
-                    </div>
-                    <div className='header-buttons__icon'>
-                    <img src={ iconMenu } alt='right icon' />
-                    </div>
-                </div>
-                <div className='header-nav'>
-                    <ul className='header-nav__menu'>
-                        <li className='header-nav__item'>OVERVIEW</li>
-                        <li className='header-nav__item header-nav__item--selected'>STRAIN</li>
-                        <li className='header-nav__item'>RECOVERY</li>
-                        <li className='header-nav__item'>SLEEP</li>
-                    </ul>
-                </div>
-            </header>
-        )
-    }
+	render() {
+		return (
+			<header className="header">
+				<div className="header-buttons">
+					<Button icon={iconChevronDown} onClick={this.onBtnClick} />
+					<div className="header-buttons__dates">
+						<div>
+							TODAY{" "}
+							<img
+								className="header-buttons__icon"
+								src={iconChevronDown}
+								alt="down"
+							/>
+						</div>
+					</div>
+					<div className="header-buttons__icon">
+						<img src={iconMenu} alt="right icon" />
+					</div>
+				</div>
+				<div className="header-nav">
+					<ul className="header-nav__menu">
+						<li className="header-nav__item">OVERVIEW</li>
+						<li className="header-nav__item header-nav__item--selected">
+							STRAIN
+						</li>
+						<li className="header-nav__item">RECOVERY</li>
+						<li className="header-nav__item">SLEEP</li>
+					</ul>
+				</div>
+			</header>
+		);
+	}
 }
--- client/src/components/button/Button.jsx
+++ client/src/components/button/Button.jsx
@@ -1,7 +1,25 @@
-export default function Button({ text, icon, onClick }) {
+import "./Button.scss";
+
+export default function Button({
+	text,
+	icon,
+	onClick,
+	btnStyle,
+	containerStyle,
+}) {
 	return (
-		<div>
-			<button>{text}</button>
+		<div
+			className={`button__container ${containerStyle ? containerStyle : ""}`}
+		>
+			<button
+				onClick={onClick}
+				className={`button ${btnStyle ? btnStyle : ""} ${
+					text ? "button--text" : ""
+				}`}
+			>
+				{text ? text : ""}
+				{!icon ? <></> : <img src={icon} alt="icon" className="button__icon" />}
+			</button>
 		</div>
 	);
 }
