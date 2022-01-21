8ab925d566e74b2becc72fe96a6ebca667f27496
Louis Flores <louisdefleurs@gmail.com>
2022-01-21 10:14:52 -0500

    fixed card

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 // app styles & assets
 import './Card.scss';
+import hide from '../../assets/Icons/hide.svg';
+;
+
 
 export default class Card extends Component {
   render() {
     return (
-        <div className='card'>
-            <div className='card-header'>
-                <img className='card-header__icon' src={ this.props.icon } alt='' />
-                <div className='card-header__text'>{ this.props.header }</div>
-            </div>
-            <div className='card-content'>{ this.props.content }</div>
+      <div className='card'>
+        <div className="card__triangle">
         </div>
+        <img src={hide} alt="icon" className="card__triangle__icon" />
+
+        <div className='card-header'>
+          <img className='card-header__icon' src={this.props.icon} alt='icon' />
+          <div className='card-header__text'>{this.props.header}</div>
+        </div>
+        <div className='card-content'>{this.props.content}</div>
+      </div>
     );
   }
 }
