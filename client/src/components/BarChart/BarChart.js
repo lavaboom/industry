f308150f487352a654690a01861ab12c9b5eea00
brynfrayne <91647976+brynfrayne@users.noreply.github.com>
2022-01-20 21:56:08 -0800

    added basic bar chart with hard coded values corresponding to mockup

---
+++ client/src/components/BarChart/BarChart.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import { Chart, registerables } from 'chart.js';
+import "./BarChart.scss";
+Chart.register(...registerables);
+
+const colors = {green:'rgba(13,210,13,.64)', yellow:'rgba(210, 210, 13,.64)',red:'rgba(210,13,13,.8)'};
+const data = {
+    labels: ['Fri', 'Sat', 'Sun',
+             'Mon', 'Tue', 'Wed', 'Thu'],
+    datasets: [
+      {
+        label: 'Recovery',
+        backgroundColor: [colors.green, colors.yellow,colors.green,colors.green,colors.green,colors.green],
+        borderWidth: 0,
+        data: [80, 65, 70, 85, 90, 92, 89]
+      }
+    ]
+  };
+  const options = {
+ 
+    
+   
+    barThickness:16,
+  }
+
+export default function BarChart() {
+  return <div className="bar-chart">
+      <Bar
+          data={data}
+          options={options}
+        />
+  </div>;
+}
--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -4,12 +4,14 @@ import React, { Component } from 'react'
 import './HomePage.scss'
 // other sub components
 import NavBarBottom from '../../components/NavBarBottom/NavBarBottom'
+import BarChart from '../../components/BarChart/BarChart'
 
 export default class HomePage extends Component {
     render() {
         return (
             <div>
                 <p>This is Home Page</p>
+                <BarChart />
                 <NavBarBottom />
             </div>
         )
